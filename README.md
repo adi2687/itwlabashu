309a07929072cae9c4fb1150356556ea0839660d
shicrajaku <adityaurani87@gmail.com>
2024-11-16 11:46:03 +0530

    chnage

---
+++ README.md
@@ -0,0 +1,1 @@
+the repo is about the 5 sorting tehcniques whihc is in the directory through the git methods
--- bubble.c
+++ bubble.c
@@ -1,4 +1,4 @@
-﻿// #include <stdio.h>
+﻿#include <stdio.h>
 
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
@@ -13,13 +13,12 @@ void bubbleSort(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {64, 34};
+    int arr[] = {64, 34, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
     printf("The arr is now sorted :  ");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     return 0;
-    // this is the new thing in new branch
 }
 
